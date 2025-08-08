f1e0263501aa1a4801bad275bce50140e7c99ac0
strong <13928983344@139.com>
2025-08-08 15:55:11 +0800

    dashboard: fix Next special files naming for SSR/SSG (_app.tsx, _document.tsx) and remove .page variants to avoid no-document-import-in-page

---
+++ packages/dashboard/src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import "../components/rangeCalendar/aria.css";
+import "../styles/time-field-aria.css";
+
+import App from "../components/app";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return <App Component={Component} pageProps={pageProps} router={router} />;
+}
+
+export default MyApp;
---
+++ packages/dashboard/src/pages/_document.tsx
@@ -0,0 +1,101 @@
+import createEmotionServer from "@emotion/server/create-instance";
+import backendConfig from "backend-lib/src/config";
+import { AppType } from "next/app";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+import { DittofeedAppProps } from "../components/app";
+import createEmotionCache from "../lib/createEmotionCache";
+
+export interface DittofeedDocumentProps extends DocumentInitialProps {
+  emotionStyleTags: React.ReactNode[];
+  appVersion?: string;
+}
+
+export default function DittofeedDocument({
+  emotionStyleTags,
+  appVersion,
+}: DittofeedDocumentProps) {
+  return (
+    <Html>
+      <Head>
+        {/* Code seems right. Might be a result of using page extensions nextjs option. */}
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap"
+        />
+        <meta name="emotion-insertion-point" content="" />
+        {/* disable indexing the dashboard */}
+        <meta name="robots" content="noindex, nofollow" />
+        {/* Hidden app version meta tag */}
+        {appVersion && (
+          <meta name="dittofeed-app-version" content={appVersion} />
+        )}
+        {emotionStyleTags}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+DittofeedDocument.getInitialProps = async (
+  ctx: DocumentContext,
+): Promise<DittofeedDocumentProps> => {
+  const originalRenderPage = ctx.renderPage;
+
+  // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (
+        DittofeedApp: AppType | React.ComponentType<DittofeedAppProps>,
+      ) =>
+        function EnhanceApp(props) {
+          return <DittofeedApp emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents Emotion to render invalid HTML.
+  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  let appVersion: string | undefined;
+  try {
+    // Get app version from backend config
+    const { appVersion: configAppVersion } = backendConfig();
+    appVersion = configAppVersion;
+  } catch (error) {
+    // If config is not available (e.g., in some build contexts), just continue without it
+    console.warn("Unable to get app version from config:", error);
+  }
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+    appVersion,
+  };
+};
